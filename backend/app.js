ada5aa55fb5d04689e6665711c65b0a4c10e7c5b
edwin <emash90@gmail.com>
2022-06-25 10:58:36 +0300

    deployment

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
 //     next();
 //   });
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+} else {
+    app.get("/", (req, res) => {
+        res.send('set to "production"');
+    });
+}
 // app.get("*", (req, res) =>
 //     res.sendFile(
 //         path.resolve(__dirname, "../", "frontend", "build", "index.html")
