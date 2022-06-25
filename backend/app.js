d739b009e0bb4557f342c7bc4fd5a0f16a8949e6
edwin <emash90@gmail.com>
2022-06-25 10:53:22 +0300

    deployment

--- backend/app.js
+++ backend/app.js
@@ -19,11 +19,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //     next();
 //   });
 app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", (req, res) =>
-    res.sendFile(
-        path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-);
+// app.get("*", (req, res) =>
+//     res.sendFile(
+//         path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+// );
 
 
 //defining out listening port
